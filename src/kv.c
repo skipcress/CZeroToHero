5031ded89e0d21cea159be6b86df4aa6f3632f6e
Kirk Sealls <kirk.sealls@gmail.com>
2026-04-26 19:34:45 -0700

    Key Value Put Test

--- src/kv.c
+++ src/kv.c
@@ -1,6 +1,22 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 #include "kv.h"
+
+#define DEAD 0x1
+
+size_t hash(char *key, size_t capacity) {
+	size_t hash = 0x123456789;
+	while(*key) {
+		hash ^= *key;
+		hash = hash << 8;
+		hash += *key;
+
+		key++;
+	}
+
+	return hash % capacity;
+} 
 
 kv_t *kv_init(size_t capacity) {
 	if(0 == capacity) {
@@ -29,4 +45,53 @@ void kv_free(kv_t *table) {
 
 	free(table);
 	table = NULL;
-}	
+}
+
+// Returns index of the key, otherwise -1
+size_t kv_put(kv_t *table, char *key, char *value) {
+	if(!key || !value) {
+		return -1;
+	}
+
+	size_t index = hash(key, table->capacity);
+
+	for(int i = 0; i < table-> capacity; i++) {
+		size_t walking_index = (index + i) % table->capacity;
+
+		kv_entry_t *entry = &table->entries[walking_index];
+		if(entry->key
+			&& !strcmp(entry->key, key)
+			&& entry->key != (void*)DEAD) {
+			char *newVal = strdup(value);
+
+			if(!newVal) {
+				return -1;
+			}
+
+			entry->value = newVal;
+
+			return walking_index;
+		}
+
+		if(!entry->key || entry->key == (void*)DEAD) {
+			char *newVal = strdup(value);
+			char *newKey = strdup(key);
+
+			if(!newVal || !newKey) {
+				return -1;
+				free(newVal);
+				free(newKey);
+			}
+
+
+			entry->key = newKey;
+			entry->value = newVal;
+
+			table->count++;
+
+			return walking_index;
+		}
+	}
+
+	return -2;
+}
--- src/main.c
+++ src/main.c
@@ -7,5 +7,19 @@ int main() {
 	assert(NULL != table);
 	assert(16 == table->capacity);
 	assert(0 == table->count);
+	
+	kv_put(table, "key", "value");
+	kv_put(table, "key", "value1");
+	kv_put(table, "key2", "value2");
+
+	for(int i = 0; i < table->capacity; i++) {
+		if(!(table->entries[i].key)) {
+			continue;
+		}
+
+		printf("KEY: %s", table->entries[i].key);
+		printf("VALUE: %s\n", table->entries[i].value);
+	}
+
 	kv_free(table);
 }
